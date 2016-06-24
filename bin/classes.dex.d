4fc1adbb2b66fe3bc40075afac53736ea0fdb450
markafarrell <mark.andrew.farrell@gmail.com>
2016-06-24 15:43:26 +1000

    Delete classes.dex.d

--- bin/classes.dex.d
+++
@@ -1,2 +0,0 @@
-D:\Users\d384492\Documents\Projects\CDRX\Battery Testing\Android\BatteryTest\bin\classes.dex : \
-D:\Users\d384492\Documents\Projects\CDRX\Battery Testing\Android\BatteryTest\bin\classes\com\maf\batterytest\BatteryTest.class \
